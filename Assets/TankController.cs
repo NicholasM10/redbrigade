e86f217bf7394a55ca0e94c85b2602694db924ec
Nick M <ndplay@ymail.com>
2018-10-06 14:24:05 +0400

    Unity project going in

---
+++ Assets/TankController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour {
+
+    public float tankSpeed = 5f;
+
+    Rigidbody2D rb;
+
+    public bool dir = true; //is facing to right
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        rb.AddForce(new Vector2(tankSpeed, 0));
+	}
+}
