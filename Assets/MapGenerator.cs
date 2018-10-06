2a74de2e129e50cc9a3a217a8f2a68718cb78b50
Nick M <ndplay@ymail.com>
2018-10-06 14:39:31 +0400

    Base mapgenerator script (empty)

---
+++ Assets/MapGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject lastTileGenerated;
+
+    public float maxIncline = 0.35f;
+
+	void Start () {
+        player = FindObjectOfType<TankController>().gameObject;
+	}
+	
+	void Update () {
+		
+	}
+}
