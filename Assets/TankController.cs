a37510dad39c303df7a3e86ea17f81f173885f82
Nick M <ndplay@ymail.com>
2018-10-06 16:07:02 +0400

    Inclines movement

--- Assets/TankController.cs
+++ Assets/TankController.cs
@@ -6,18 +6,52 @@ public class TankController : MonoBehaviour {
 
     public float tankSpeed = 5f;
 
+    public float climbRate = 5f;
+    public float climbDistance = 0.5f;
+
+    public LayerMask ignoreLayer;
+
     Rigidbody2D rb;
 
     public bool dir = true; //is facing to right
 
+    public Transform[] raycastTransforms;
+
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         rb.AddForce(new Vector2(tankSpeed, 0));
+
+        CheckClimb();
 	}
+
+    void ClimbForce()
+    {
+        rb.AddForce(new Vector2(0, climbRate));
+    }
+
+    void CheckClimb()
+    {
+
+        Debug.DrawRay(raycastTransforms[0].transform.position, Vector2.right);
+        if (Physics2D.Raycast(raycastTransforms[0].transform.position, Vector2.right, climbDistance, ignoreLayer))
+        {
+            if (Physics2D.Raycast(raycastTransforms[1].transform.position, Vector2.right, climbDistance, ignoreLayer))
+            {
+
+            }
+            else
+            {
+                ClimbForce();
+                //Todo: do animation here (rotation)
+            }
+        }
+    }
 }
