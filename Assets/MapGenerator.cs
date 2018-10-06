2ce94fceadeca1fb77924908c75760cdf27aad27
Nick M <ndplay@ymail.com>
2018-10-06 15:30:45 +0400

    BASIC Map generator

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -6,6 +6,9 @@ public class MapGenerator : MonoBehaviour {
 
     public GameObject player;
     public GameObject lastTileGenerated;
+    public float distanceFromPlayer;
+
+    public GameObject tilePrefab;
 
     public float maxIncline = 0.35f;
 
@@ -13,7 +16,20 @@ public class MapGenerator : MonoBehaviour {
         player = FindObjectOfType<TankController>().gameObject;
 	}
 	
-	void Update () {
-		
+	void Update ()
+    {
+        distanceFromPlayer = Vector2.Distance(player.transform.position, lastTileGenerated.transform.position);
+
+        if(distanceFromPlayer < 7.5)
+        {
+            GameObject obj = Instantiate(tilePrefab, lastTileGenerated.transform.position + new Vector3(1, GetIncline(), 0), Quaternion.identity);
+            obj.transform.SetParent(gameObject.transform);
+            lastTileGenerated = obj;
+        }
 	}
+
+    float GetIncline() //Get incline or downcline
+    {
+        return Random.Range(-maxIncline, maxIncline);
+    }
 }
