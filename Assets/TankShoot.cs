3d400eebf7741975b6899f3161211ac0d54a6f0b
Nick M <ndplay@ymail.com>
2018-10-07 11:49:40 +0400

    Basic shooting, enemies, and other stuff

---
+++ Assets/TankShoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankShoot : MonoBehaviour {
+
+    public GameObject[] projectilePrefabs;
+
+    public Transform turretPos;
+
+    public float bulletForce = 5f;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject obj = Instantiate(projectilePrefabs[0], turretPos.position, Quaternion.identity);
+            obj.GetComponent<Rigidbody2D>().AddForce(new Vector2(bulletForce, 0));
+        }
+    }
+}
